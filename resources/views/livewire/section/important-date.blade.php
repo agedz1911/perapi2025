e28828594140e7f0b1e1f93019f86e68f1c21ffa
Asep Saepuloh <mac@macs-MacBook-Pro.local>
2025-03-15 11:45:06 +0700

    rev important date

--- resources/views/livewire/section/important-date.blade.php
+++ resources/views/livewire/section/important-date.blade.php
@@ -6,9 +6,9 @@
                 class="text-gray-500 justify-between font-normal hover:text-purple-700 after:content-[''] relative after:absolute flex items-center hover:after:items-center transition-all after:transition-all duration-300 after:duration-300 after:bg-purple-600 hover:ps-3 after:left-0 after:h-0 hover:after:h-[5px] after:w-0 hover:after:w-[5px] after:rounded-full">
                 {{$date->title}} : {{
                 $date->date_end
-                ? \Carbon\Carbon::parse($date->date)->format('d F') . ' - ' .
-                \Carbon\Carbon::parse($date->date_end)->format('d F, Y')
-                : \Carbon\Carbon::parse($date->date)->format('d F, Y')
+                ? \Carbon\Carbon::parse($date->date)->format('l, d F') . ' - ' .
+                \Carbon\Carbon::parse($date->date_end)->format('l, d F, Y')
+                : \Carbon\Carbon::parse($date->date)->format('l, d F, Y')
                 }}
             </a>
         </li>
