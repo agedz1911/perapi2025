47280e02bf852cfa11ca46f30e3735a9a1832d43
Asep Saepuloh <mac@macs-MacBook-Pro.local>
2025-03-28 14:33:22 +0700

    initial development

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -24,7 +24,7 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
-            // ->registration()
+            ->registration()
             ->passwordReset()
             ->emailVerification()
             ->profile()
